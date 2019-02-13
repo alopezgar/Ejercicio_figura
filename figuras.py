4d3b7076e4caf0611311e55a48b536036a405fc9
alopezgar <46898706+alopezgar@users.noreply.github.com>
2019-02-13 15:50:16 +0100

    Update figuras.py

--- figuras.py
+++ figuras.py
@@ -12,7 +12,6 @@ class Cuadrado(Forma):
     def __init__(self,lado):
         self.lado = lado
 
-
     def get_area(self):
         area = self.lado*self.lado
         return area
@@ -21,6 +20,38 @@ class Cuadrado(Forma):
         perimetro = 4*self.lado
         return perimetro
 
+class Rectangulo(Forma):
+    def __init__(self,lado1,lado2):
+        self.lado1 = lado1
+        self.lado2 = lado2
+     
+    def get_area(self):
+        area = lado1 * lado2
+        return area
+    
+    def get_perimetro(self):
+        perimetro = 2*lado1 + 2*lado2
+        return perimetro
+    
+ class Circulo(Forma):
+    def __init__(self,radio):
+        self.radio = radio
+    
+    def get_area(self):
+        area = math.pi*radio**2
+        return area
+    def get_perimetro(self):
+        perimetro = 2*math.pi*radio
+        return perimetro
 
-valor = Cuadrado(3)
-print(valor.get_area())
+a = Cuadrado(3)
+print(a.get_area())
+print(a.get_perimetro())
+
+b = Rectangulo(3,4)
+print(b.get_area())
+print(b.get_perimetro())
+
+c = Circulo(2)
+print(c.get_area())
+print(c.get_perimetro())
